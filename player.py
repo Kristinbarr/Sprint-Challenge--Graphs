34525ed4e9acf9a52829e175ec0a0e5bd689bb62
Kristin Barr <kristinbarr@gmail.com>
2020-04-16 17:21:02 -0400

    player notes

--- player.py
+++ player.py
@@ -1,7 +1,13 @@
 class Player:
     def __init__(self, starting_room):
         self.current_room = starting_room
+
     def travel(self, direction, show_rooms = False):
+        ''' 
+        takes a direction, moves the player
+        also take arg show_rooms option, default to false
+        if no room in that direction, returns error message
+        '''
         next_room = self.current_room.get_room_in_direction(direction)
         if next_room is not None:
             self.current_room = next_room
