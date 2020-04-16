fa6c7691016b17de160bf5040f51247a6397ec55
Kristin Barr <kristinbarr@gmail.com>
2020-04-16 17:21:29 -0400

    adv graph WIP

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue
 
 import random
 from ast import literal_eval
@@ -8,13 +9,12 @@ from ast import literal_eval
 # Load world
 world = World()
 
-
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -22,12 +22,50 @@ world.load_graph(room_graph)
 
 # Print an ASCII map
 world.print_rooms()
-
+# creates the current player
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
+# traversal_path = ['n','n', 's', 's', 's', 's', 'n','n','e', 'e', 'w', 'w', 'w', 'w']
 # traversal_path = ['n', 'n']
 traversal_path = []
+
+# generate a random direction [n, s, e, w]
+def random_direction():
+    dirs = ['n', 's', 'e', 'w']
+    return random.choice(dirs)
+
+# CONSTRUCT A TRAVEL GRAPH
+# bft?
+# starts in room { 0: {'n':'?', 's':'?', 'w':'?', 'e':'?'} }
+# create queue
+q = Queue()
+# enqueue first room id
+q.enqueue(player.current_room.id)
+
+# loop while queue is not empty
+while q.size() > 0:
+    # dequeue first room
+    room = q.dequeue()
+    print('room:', room)
+    # pick a random unexplored direction from cur room, unexplored = '?'
+    direction = random_direction()
+    print('direction:', direction)
+    print('room in direction:', player.current_room.get_room_in_direction(direction))
+    # if room in that direction has not been visited aka ?,
+    if player.current_room.get_room_in_direction(direction) != None:
+        print('here')
+        # travel to that room
+        player.travel(direction)
+        # log that direction
+        traversal_path.append(direction)
+
+        unvisited = player.current_room.get_exits()
+        print('unvisited:', unvisited)
+
+        # enqueue all unvisited rooms
+
+
 
 
 
@@ -47,10 +85,7 @@ else:
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
-#######
-# UNCOMMENT TO WALK AROUND
-#######
+# TO WALK AROUND
 player.current_room.print_room_description(player)
 while True:
     cmds = input("-> ").lower().split(" ")
