ae2bdc952e1e58d1d2a71f3db385c0ee90209577
Kristin Barr <kristinbarr@gmail.com>
2020-05-03 02:22:35 -0400

    WIP

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from graph import Graph
 
 import random
 from ast import literal_eval
@@ -8,13 +9,12 @@ from ast import literal_eval
 # Load world
 world = World()
 
-
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -22,13 +22,52 @@ world.load_graph(room_graph)
 
 # Print an ASCII map
 world.print_rooms()
-
+# create a new player. room_graph will start player at room 0
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
 traversal_path = []
+# traversal_path = ['n','n']
+# traversal_path = ['n','n','s','s','s','s','n','n','e','e','w','w','w','w']
 
+
+# BUILD A GRAPH, add to traveral_path similtaneously during building
+
+# instantiate graph
+graph = Graph()
+
+# room_graph: {
+#     0: [ (3, 5), {'n': 1} ], 
+#     1: [ (3, 6), {'s': 0, 'n': 2} ], 
+#     2: [ (3, 7), {'s': 1} ],
+# }
+# graph: { 
+#   0:{'n':1}, 
+#   1:{'s':0,'n':2}, 
+#   2:{'s':1} 
+# }
+
+# iterate through room_graph
+for room_id in room_graph:
+    # check if room is present
+    if room_id not in graph.rooms:
+        # if not, insert room - do all exits get inserted?
+        graph.add_room(room_id, room_graph[room_id][1])
+    
+
+    # generate random exit direction
+    directions = ['n','s','e','w']
+    # print([random.randint(0,3) for i in directions])
+
+    # iterate over exits
+        # check if its a valid move
+        # add exit direction to traversal list
+        # add exit to the graph
+
+# functions to use:
+# player.current_room.id
+# player.current_room.get_exits()
+# player.travel(direction)
 
 
 # TRAVERSAL TEST
@@ -47,10 +86,7 @@ else:
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
-#######
-# UNCOMMENT TO WALK AROUND
-#######
+# WALK AROUND
 player.current_room.print_room_description(player)
 while True:
     cmds = input("-> ").lower().split(" ")
